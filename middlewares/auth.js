386c704731d0f2ac951cd11367fb4bd2946e104f
Somaditya-Bindhani <somaditya.bindhani136@gmail.com>
2022-08-11 18:27:25 +0530

    config

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,25 @@
 const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 
-const auth = (req,res,next)=>{
-    if(req.method==="OPTIONS"){
-        return next();
-    };
+const auth = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
 
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        if(!token)
-      {
-        return next(new HttpError("Auth failed",402));
-      }  else{
-     
-            const decodedToken = jwt.verify(token,"lolakslddfjsfbmncvmencmdsjsndjcmwtfrohanpoopsurgecnanda");
-            req.userData = {
-                userId:decodedToken.id
-            }
-            next(); 
-    }
-}catch(err){
-        return next(new HttpError("Auth failed",402));
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return next(new HttpError("Auth failed", 402));
+    } else {
+      const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+      req.userData = {
+        userId: decodedToken.id,
       };
- 
-  
-}
+      next();
+    }
+  } catch (err) {
+    return next(new HttpError("Auth failed", 402));
+  }
+};
 
 module.exports = auth;
